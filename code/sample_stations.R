65c9d953ffad4dc38afeca0dbd660cf5809b9832
mcsiple-NOAA <73858992+mcsiple-NOAA@users.noreply.github.com>
2020-11-27 09:45:04 -0800

    styler::stylize()

--- code/sample_stations.R
+++ code/sample_stations.R
@@ -11,145 +11,179 @@ library(sf)
 
 
 # 1. Load helpers ---------------------------------------------------------
-# Palette lengthener function 
-source(here::here("code","lengthen_pal.R"))
+# Palette lengthener function
+source(here::here("code", "lengthen_pal.R"))
 
 
 # 2. Load optimized survey design -----------------------------------------
 
 # * Spatial grid ----------------------------------------------------------
-load(here::here("..","Optimal_Allocation_GoA-master",
-                "data",
-                "Extrapolation_depths.RData"))
-#dataframe: Extrapolation_depths
+load(here::here(
+  "..", "Optimal_Allocation_GoA-master",
+  "data",
+  "Extrapolation_depths.RData"
+))
+# dataframe: Extrapolation_depths
 
 # * Load Optimization Results ---------------------------------------------
-load(here::here("..","Optimal_Allocation_GoA-master",
-                "model_11",
-                "full_domain",
-                "Spatiotemporal_Optimization",
-                "optimization_knitted_results.RData"))
+load(here::here(
+  "..", "Optimal_Allocation_GoA-master",
+  "model_11",
+  "full_domain",
+  "Spatiotemporal_Optimization",
+  "optimization_knitted_results.RData"
+))
 # df's and lists: res_df, settings, strata_list, strata_stats_list
 
 # 3. Pick solution and get survey information -----------------------------
 # * 3.1 Query which solution to use based on 1-boat, 15 strata solution----
 # Important! Number of boats.
-nboats = 3
+nboats <- 3
 
 idx <- settings$id[which(settings$strata == 15 & settings$boat == nboats)]
 
 # * 3.2 Extract solution --------------------------------------------------
-strata_no <- as.numeric(as.character(strata_list[[idx]]$Stratum)) #unique stratum "ID"
-nh <- strata_list[[idx]]$Allocation #allocated effort across strata (n of locations to visit)
+strata_no <- as.numeric(as.character(strata_list[[idx]]$Stratum)) # unique stratum "ID"
+nh <- strata_list[[idx]]$Allocation # allocated effort across strata (n of locations to visit)
 nstrata <- length(nh)
-solution <- res_df[, paste0("sol_", idx)] #Optimized solution
+solution <- res_df[, paste0("sol_", idx)] # Optimized solution
 
 
 # * 3.3 Take a stratified random sample -----------------------------------
-#Loop across strata, take a random sample based on how many were allocated
+# Loop across strata, take a random sample based on how many were allocated
 nrow(Extrapolation_depths)
 length(solution)
 
 sample_vec <- c()
 for (istrata in 1:nstrata) {
-  sample_vec <- c(sample_vec,
-                  sample(x = which(solution == strata_no[istrata]),
-                         size = nh[istrata]))
+  sample_vec <- c(
+    sample_vec,
+    sample(
+      x = which(solution == strata_no[istrata]),
+      size = nh[istrata]
+    )
+  )
 }
 
-# Subset the spatial grid to only those that were sampled 
+# Subset the spatial grid to only those that were sampled
 # Includes trawlable and non-trawlable locations
-Extrapolation_depths[sample_vec,]
+Extrapolation_depths[sample_vec, ]
 
 
 # Shortcut to plot solution and simulated survey locations
 goa <- sp::SpatialPointsDataFrame(
-  coords = Extrapolation_depths[,c("E_km", "N_km")],
-  data = data.frame(solution = solution) )
+  coords = Extrapolation_depths[, c("E_km", "N_km")],
+  data = data.frame(solution = solution)
+)
 
 
-goa_ras <- raster::raster(x = goa,
-                          resolution = 5)
-goa_ras <- raster::rasterize(x = goa,
-                             y = goa_ras,
-                             field = "solution")
+goa_ras <- raster::raster(
+  x = goa,
+  resolution = 5
+)
+goa_ras <- raster::rasterize(
+  x = goa,
+  y = goa_ras,
+  field = "solution"
+)
 
-strata_pal <- lengthen_pal(x = 1:nstrata,
-                           shortpal = brewer.pal(9,"Pastel1"))
+strata_pal <- lengthen_pal(
+  x = 1:nstrata,
+  shortpal = brewer.pal(9, "Pastel1")
+)
 
 plot(goa_ras,
-     col = strata_pal)
+  col = strata_pal
+)
 
-points(Extrapolation_depths[sample_vec,
-                            c("E_km", "N_km")],
-       pch = 16,
-       cex = 0.5)
+points(Extrapolation_depths[
+  sample_vec,
+  c("E_km", "N_km")
+],
+pch = 16,
+cex = 0.5
+)
 
 
 # * 3.4 Get sampled points ------------------------------------------------
-survey_pts <- Extrapolation_depths[sample_vec,
-                                   c("Lon", "Lat","E_km", "N_km",
-                                     "Id","stratum","trawlable")]
+survey_pts <- Extrapolation_depths[
+  sample_vec,
+  c(
+    "Lon", "Lat", "E_km", "N_km",
+    "Id", "stratum", "trawlable"
+  )
+]
 
-field_sf <- sf::st_as_sf(x = Extrapolation_depths,
-                         coords = c("Lon","Lat"),
-                         crs = 4326)
+field_sf <- sf::st_as_sf(
+  x = Extrapolation_depths,
+  coords = c("Lon", "Lat"),
+  crs = 4326
+)
 
 # Assign stations to different boats in the survey
-if(nboats==2){
+if (nboats == 2) {
   print("2-boat solution")
-  nperboat <- nrow(survey_pts)/2
-  if(!is.integer(nperboat)){
-    n1 = floor(nperboat)
-    n2 = ceiling(nperboat)
-  }else (n1 = n2 = nperboat)
-  bb <- c(rep(1,times = n1),rep(2,times = n2))
-  survey_pts <- survey_pts %>% 
-    mutate(whichboat = sample(x = bb, size = n1+n2, replace = FALSE))
+  nperboat <- nrow(survey_pts) / 2
+  if (!is.integer(nperboat)) {
+    n1 <- floor(nperboat)
+    n2 <- ceiling(nperboat)
+  } else {
+    (n1 <- n2 <- nperboat)
+  }
+  bb <- c(rep(1, times = n1), rep(2, times = n2))
+  survey_pts <- survey_pts %>%
+    mutate(whichboat = sample(x = bb, size = n1 + n2, replace = FALSE))
 }
 
-if(nboats==3){
+if (nboats == 3) {
   print("3-boat solution")
-  nperboat <- nrow(survey_pts)/3
-  if(!is.integer(nperboat)){
-    n1 = floor(nperboat)
-    n2 = ceiling(nperboat)
-    n3 = nrow(survey_pts) - (n1+n2)
-  }else (n1 = n2 = n3 = nperboat)
-  bb <- c(rep(1,times = n1),
-          rep(2,times = n2),
-          rep(3,times = n3))
-  survey_pts <- survey_pts %>% 
-    mutate(whichboat = sample(x = bb, size = n1+n2+n3, replace = FALSE))
+  nperboat <- nrow(survey_pts) / 3
+  if (!is.integer(nperboat)) {
+    n1 <- floor(nperboat)
+    n2 <- ceiling(nperboat)
+    n3 <- nrow(survey_pts) - (n1 + n2)
+  } else {
+    (n1 <- n2 <- n3 <- nperboat)
+  }
+  bb <- c(
+    rep(1, times = n1),
+    rep(2, times = n2),
+    rep(3, times = n3)
+  )
+  survey_pts <- survey_pts %>%
+    mutate(whichboat = sample(x = bb, size = n1 + n2 + n3, replace = FALSE))
 }
 
 # Turn survey points into sf object for getting distances
-survey_sf <- sf::st_as_sf(x = survey_pts,
-                          coords = c("Lon","Lat"),
-                          crs = 4326, agr = "constant")
+survey_sf <- sf::st_as_sf(
+  x = survey_pts,
+  coords = c("Lon", "Lat"),
+  crs = 4326, agr = "constant"
+)
 
 # Pairwise distances between survey points in km
 distance_matrix_km <- matrix(as.numeric(st_distance(survey_sf) / 1000),
-                             nrow = length(sample_vec)) 
+  nrow = length(sample_vec)
+)
 
-rownames(distance_matrix_km) <- 
-  colnames(distance_matrix_km) <- 
+rownames(distance_matrix_km) <-
+  colnames(distance_matrix_km) <-
   survey_sf$Id
 
-#save(distance_matrix_km,file = here::here("data","DistanceMatrix1.RData"))
-#load(here::here("data","DistanceMatrix1.RData"))
+# save(distance_matrix_km,file = here::here("data","DistanceMatrix1.RData"))
+# load(here::here("data","DistanceMatrix1.RData"))
 
 # Long-form distance matrix
-distance_df <- as.data.frame(distance_matrix_km) %>% 
+distance_df <- as.data.frame(distance_matrix_km) %>%
   add_column(surveyId = colnames(distance_matrix_km)) %>%
   pivot_longer(cols = colnames(distance_matrix_km))
 
 
-#4.  Calculate total survey time ------------------------------------------
-#According to N Laman, on average each boat does 4.7 tows/day
+# 4.  Calculate total survey time ------------------------------------------
+# According to N Laman, on average each boat does 4.7 tows/day
 # Roughest estimate (274 survey points)
-nrow(survey_pts)/4.2 #Wayne
-nrow(survey_pts)/4.7 #Ned
+nrow(survey_pts) / 4.2 # Wayne
+nrow(survey_pts) / 4.7 # Ned
 
 # Sample each station, starting from furthest west point and sampling the nearest station next:
 west_to_east <- survey_pts %>%
@@ -158,7 +192,7 @@ western_end <- west_to_east %>%
   slice(1)
 
 # Check that this is the westernmost survey point:
-points(western_end[,c("E_km", "N_km")],col="red")
+points(western_end[, c("E_km", "N_km")], col = "red")
 
 
 
@@ -167,59 +201,65 @@ points(western_end[,c("E_km", "N_km")],col="red")
 
 # * 4.1 Option 1: Prioritize next station by proximity, depth, and W-to-E ----
 
-source(here::here("code","stationdecisions","get_next_station_1.R"))
+source(here::here("code", "stationdecisions", "get_next_station_1.R"))
 
 test.id <- as.character(survey_sf$Id[1])
 
-x <- get_next_station_1(stationId = western_end$Id,already_sampled = NA)
-points(filter(survey_pts,Id==x)[,c("E_km", "N_km")],col = 'blue')
+x <- get_next_station_1(stationId = western_end$Id, already_sampled = NA)
+points(filter(survey_pts, Id == x)[, c("E_km", "N_km")], col = "blue")
 
-y <- get_next_station_1(stationId = x,already_sampled = c(western_end$Id, x))
-points(filter(survey_pts,Id==y)[,c("E_km", "N_km")],col = 'green')
+y <- get_next_station_1(stationId = x, already_sampled = c(western_end$Id, x))
+points(filter(survey_pts, Id == y)[, c("E_km", "N_km")], col = "green")
 
-z <- get_next_station_1(stationId = y,already_sampled = c(western_end$Id, x, y))
-points(filter(survey_pts,Id==z)[,c("E_km", "N_km")],col = 'yellow')
+z <- get_next_station_1(stationId = y, already_sampled = c(western_end$Id, x, y))
+points(filter(survey_pts, Id == z)[, c("E_km", "N_km")], col = "yellow")
 
 # Setup survey plan
 plan1 <- rep(NA, times = length(sample_vec))
 plan1[1] <- western_end$Id
 
-for(i in 2:length(sample_vec)){
-  plan1[i] <- get_next_station_1(stationId = plan1[i-1],
-                                 already_sampled = plan1[1:i])
+for (i in 2:length(sample_vec)) {
+  plan1[i] <- get_next_station_1(
+    stationId = plan1[i - 1],
+    already_sampled = plan1[1:i]
+  )
 }
 
 d1 <- data.frame(Id = plan1, nwd_order = 1:length(plan1))
 
 d2 <- Extrapolation_depths %>%
   mutate(Id = as.character(Id)) %>%
-  right_join(d1,Extrapolation_depths, by = "Id")
+  right_join(d1, Extrapolation_depths, by = "Id")
 
-d3 <- d2 %>% 
+d3 <- d2 %>%
   arrange(nwd_order) %>%
-  add_column(distance_from_prev = NA,
-             cumu_distance = 0)
+  add_column(
+    distance_from_prev = NA,
+    cumu_distance = 0
+  )
 d3$distance_from_prev[1] <- 0
 
-for(i in 2:nrow(d3)){
+for (i in 2:nrow(d3)) {
   d3$distance_from_prev[i] <- distance_df %>%
-    filter(surveyId == d3$Id[i], name == d3$Id[i-1]) %>%
-    dplyr::select(value) %>% 
+    filter(surveyId == d3$Id[i], name == d3$Id[i - 1]) %>%
+    dplyr::select(value) %>%
     as.numeric()
   d3$cumu_distance[i] <- sum(d3$distance_from_prev[1:i])
 }
 
 tail(d3)
 
-cat("max survey distance (km) \n",
-    max(d3$cumu_distance) )
+cat(
+  "max survey distance (km) \n",
+  max(d3$cumu_distance)
+)
 
-# Plot survey path 
+# Plot survey path
 
-attempt1 <- ggplot() + 
-  geom_sf(data = field_sf) + 
-  geom_path(data = d3, aes(x = Lon,y = Lat, colour = nwd_order)) +
-  geom_point(data = d3, aes(x = Lon,y = Lat, colour = nwd_order)) +
+attempt1 <- ggplot() +
+  geom_sf(data = field_sf) +
+  geom_path(data = d3, aes(x = Lon, y = Lat, colour = nwd_order)) +
+  geom_point(data = d3, aes(x = Lon, y = Lat, colour = nwd_order)) +
   scale_colour_viridis_c("Sampling order \n(1 = start of survey)") +
   labs(title = "Nearest neighbor / furthest west")
 
@@ -230,34 +270,34 @@ attempt1
 
 
 # * 4.2 Option 2: Use "traveling salesperson" solution --------------------
-source(here::here("code","stationdecisions","tsp.R"))
+source(here::here("code", "stationdecisions", "tsp.R"))
 
 tsp_sol <- get_tsp_soln(x = distance_matrix_km)
 
 d4 <- d3 %>%
   left_join(tsp_sol, by = c("Id" = "site"))
 
-attempt2 <- ggplot() + 
-  geom_sf(data = field_sf) + 
-  geom_path(data = d4, aes(x = Lon,y = Lat, colour = tsp_order)) +
-  geom_point(data = d4, aes(x = Lon,y = Lat, colour = tsp_order)) +
+attempt2 <- ggplot() +
+  geom_sf(data = field_sf) +
+  geom_path(data = d4, aes(x = Lon, y = Lat, colour = tsp_order)) +
+  geom_point(data = d4, aes(x = Lon, y = Lat, colour = tsp_order)) +
   scale_colour_viridis_c("Sampling order \n(1 = start of survey)") +
   labs(title = "Traveling salesperson")
 
 
 library(patchwork)
 
-#png(here::here("figures", "Attempt1_2.png"),
+# png(here::here("figures", "Attempt1_2.png"),
 #    width = 8, height = 10,
 #    units = 'in',res = 250)
-attempt1 + attempt2 + plot_layout(ncol=1)
-#dev.off()
+attempt1 + attempt2 + plot_layout(ncol = 1)
+# dev.off()
 
 
 
 # 5. Distribution of two closest stations for each station ----------------
 distance_df # distances between each pair
-# nearest_neighbor <- distance_df %>% 
+# nearest_neighbor <- distance_df %>%
 #   select(surveyId) %>%
 #   distinct() %>%
 #   mutate(nene1 = function(x) filter(distance_df, surveyId==x) %>% value)
@@ -265,29 +305,34 @@ distance_df # distances between each pair
 nearest_neighbor <- distance_df %>%
   arrange(value) %>%
   group_by(surveyId) %>%
-  summarize(nn1 = nth(value,n = 2), # min distance will always be 0
-            nn2 = nth(value,n = 3))
-nnlabs <- c("Nearest neighbor","Second-nearest neighbor")
-names(nnlabs) <- c("nn1","nn2")
+  summarize(
+    nn1 = nth(value, n = 2), # min distance will always be 0
+    nn2 = nth(value, n = 3)
+  )
+nnlabs <- c("Nearest neighbor", "Second-nearest neighbor")
+names(nnlabs) <- c("nn1", "nn2")
 
 # Plot distribution of nearest neighbors
 nnplot <- nearest_neighbor %>%
   pivot_longer(cols = nn1:nn2) %>%
   ggplot(aes(x = value, fill = name)) +
-  geom_histogram(alpha = 0.2, binwidth = 5, position = "identity") + 
+  geom_histogram(alpha = 0.2, binwidth = 5, position = "identity") +
   xlab("Distance (km)") +
   ylab("Frequency") +
   scale_fill_discrete("",
-                      labels = c("Nearest station",
-                                 "Second-nearest station"))
+    labels = c(
+      "Nearest station",
+      "Second-nearest station"
+    )
+  )
 
 
-png(filename = here::here("figures","Optimal.png"),width = 6, height = 5, units = 'in',res = 150)
+png(filename = here::here("figures", "Optimal.png"), width = 6, height = 5, units = "in", res = 150)
 nnplot
 dev.off()
 # Other notes -------------------------------------------------------------
 # 1 knot = 1.852 km/hr
 
 # Need to figure out how to make raster + map in ggplot -------------------
-#ggplot() + geom_sf(data = field_sf) 
-#ggplot(goa_ras)  + geom_raster(aes(x=))
+# ggplot() + geom_sf(data = field_sf)
+# ggplot(goa_ras)  + geom_raster(aes(x=))
