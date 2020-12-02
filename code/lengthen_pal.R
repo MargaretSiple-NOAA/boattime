662a0f7e36e97d1183cfabe86f84142ae99a1c9b
mcsiple-NOAA <73858992+mcsiple-NOAA@users.noreply.github.com>
2020-12-02 12:36:45 -0800

    add param descriptions

--- code/lengthen_pal.R
+++ code/lengthen_pal.R
@@ -1,4 +1,13 @@
-lengthen_pal <- function(x=1:10,shortpal){
+#' Title
+#'
+#' @param x a vector of 1:n, where n is the number of unique colors you want
+#' @param shortpal a short color palette, e.g., one from RColorBrewer()
+#'
+#' @return a vector of the same length as x, with n unique colors
+#' @export
+#'
+#' @examples
+lengthen_pal <- function(x = 1:10, shortpal) {
   ncolours <- length(unique(x))
   newpal <- colorRampPalette(shortpal)(ncolours)
   return(newpal)
